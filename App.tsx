f18922ff9c9bdb99329af3788e33a314078a8339
henry <tarou.88@gmail.com>
2019-11-29 21:13:06 +0900

    ✏️ eslint

--- App.tsx
+++ App.tsx
@@ -1,37 +1,24 @@
-import React from 'react';
-import { StyleSheet, StatusBar, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import Transitions from './src/Transitions';
+import Transitions from "./src/Transitions";
 
 const AppNavigator = createAppContainer(
-  createStackNavigator(
-    {
-      Transitions: {
-        screen: Transitions,
-        navigationOptions: {
-          title: "Transitions"
-        }
-      } 
+  createStackNavigator({
+    Transitions: {
+      screen: Transitions,
+      navigationOptions: {
+        title: "Transitions"
+      }
     }
-  )
-)
-
+  })
+);
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content"/>
+      <StatusBar barStyle="light-content" />
       <AppNavigator />
     </>
   );
-  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/Transitions/Transitions.tsx
+++ src/Transitions/Transitions.tsx
@@ -1,30 +1,27 @@
-import React, { useRef } from 'react';
-import { Text, View, StyleSheet } from 'react-native'; 
+import React, { useRef } from "react";
+import { Text, StyleSheet } from "react-native";
 import {
   Transitioning,
   Transition,
   TransitioningView
-} from 'react-native-reanimated';
+} from "react-native-reanimated";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white"
   }
 });
 
 const transition = (
   <Transition.Change interpolation="easeInOut" durationMs={400} />
-)
+);
 
 export default () => {
   const ref = useRef<TransitioningView>(null);
-  return(
-    <Transitioning.View
-      style={[styles.container]}
-      {...{ transition, ref }}
-    >
+  return (
+    <Transitioning.View style={[styles.container]} {...{ transition, ref }}>
       <Text>Hello</Text>
-    </Transitioning.View> 
-  )
-}
+    </Transitioning.View>
+  );
+};
