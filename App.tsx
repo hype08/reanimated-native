e47d2521ec2bd8a20e06d5b97ccb4f96d5f123b2
henry <tarou.88@gmail.com>
2019-11-29 17:39:49 +0900

    🔧dependencies
    ✨add Navigator
    🏗️add Transitions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Transitions from './src/Transitions';
+
+const AppNavigator = createAppContainer(
+  createStackNavigator(
+    {
+      Transitions: {
+        screen: Transitions,
+        navigationOptions: {
+          title: "Transitions"
+        }
+      } 
+    }
+  )
+)
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content"/>
+      <AppNavigator />
+    </>
   );
+  
 }
 
 const styles = StyleSheet.create({
---
+++ src/Transitions/Transitions.tsx
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react';
+import { Text, View, StyleSheet } from 'react-native'; 
+import {
+  Transitioning,
+  Transition,
+  TransitioningView
+} from 'react-native-reanimated';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  }
+});
+
+const transition = (
+  <Transition.Change interpolation="easeInOut" durationMs={400} />
+)
+
+export default () => {
+  const ref = useRef<TransitioningView>(null);
+  return(
+    <Transitioning.View
+      style={[styles.container]}
+      {...{ transition, ref }}
+    >
+      <Text>Hello</Text>
+    </Transitioning.View> 
+  )
+}
